a9dfff766bfe9fc281d32060ca70b6943afe7ebb
dede-pc <dede-pc@dede-pc.dede-pc>
2019-11-26 22:56:14 -0300

    insertt ypewriter-effect

--- src/components/Description.js
+++ src/components/Description.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Typewriter from 'typewriter-effect';
 
 const useStyles = makeStyles(theme => ({
   descriptionContainer: {
@@ -21,13 +22,23 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
+
 const Description = ({ question }) => {
   const classes = useStyles();
 
   return (
     <Container className={classes.descriptionContainer}>
       <div className={classes.descriptionContent}>
-        <Typography variant="subtitle1">{question}</Typography>
+        <Typography variant="subtitle1">
+          <Typewriter
+            options={{ delay: 50 }}//aumente o valor para ficar mais lento
+            onInit={(typewriter) => {
+              typewriter.typeString(question)
+                .start();
+            }}
+          />
+        </Typography>
       </div>
     </Container>
   );
